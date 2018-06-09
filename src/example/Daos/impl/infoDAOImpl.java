65e21152830a2d211da2480fa5218255521f6867
qiaoshichao <qiaoshichao@baidu.com>
2018-06-09 23:19:46 +0800

    提交了前端界面

--- src/example/Daos/impl/infoDAOImpl.java
+++ src/example/Daos/impl/infoDAOImpl.java
@@ -6,6 +6,7 @@ import example.beans.valueBeans;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import static example.utils.DBUtil.*;
@@ -19,7 +20,7 @@ public class infoDAOImpl implements IinfoDAO {
         int rowCount = 0;
         try {
             conn = getConn();
-            String sql = "update tb.info SET name=" + vo.getName() + "WHERE id=" + String.valueOf(vo.getId());
+            String sql = "update tb.info SET name=" + vo.getName() + " WHERE id=" + String.valueOf(vo.getId());
             stat = conn.prepareStatement(sql);
             rowCount = stat.executeUpdate();
 
@@ -38,18 +39,26 @@ public class infoDAOImpl implements IinfoDAO {
     public List<valueBeans> doQuery(int id) {
         Connection conn = null;
         PreparedStatement stat = null;
-        ResultSet rs=null;
+        ResultSet rs = null;
+        List<valueBeans> listValue = new ArrayList<valueBeans>();
+        valueBeans valueObject = new valueBeans();
         try {
             conn = getConn();
             String sql = "select * from tb_info where id=" + String.valueOf(id);
             stat = conn.prepareStatement(sql);
-            rs =stat.executeQuery();
-        }catch(Exception e){
+            rs = stat.executeQuery();
+            while (rs.next()) {
+                valueObject.setId(rs.getInt("id"));
+                valueObject.setName(rs.getString("name"));
+                valueObject.setArguments(rs.getString("arguments"));
+                listValue.add(valueObject);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally{
-            closeConn(rs,stat,conn);
+        } finally {
+            closeConn(rs, stat, conn);
         }
-
+        return listValue;
     }
 
 }
